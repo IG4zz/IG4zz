6584d22087720a60ac43791b74531b610e270f91
IG4zz <Flusha_04@mail.ru>
2022-03-27 17:07:43 +0300

    release ver 1.0

---
+++ Caesar Cipher/FormCrypt-Encrypt.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace CaesarCipher
+{
+    public partial class FormWork : Form
+    {
+        public FormWork()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+        }
+        bool ValidCharFound(string str)//функция проверки наличия только символовцифр и , в //строке
+        {
+            bool valid = true;// 
+           
+                 foreach (char c in str) // искать символ c в строке str
+                 {
+                    string bfr = c.ToString();
+                     if (Regex.IsMatch(bfr, @"[-0-9,]"))
+                     {
+                       valid = true;//то результат=истина
+                     }
+                     else
+                     {
+                    valid = false;//иначе ложь
+                    break;
+                     }
+                 }
+                  return valid;
+        }
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            if (textBoxKey.Text != null && textBoxKey.Text != "")
+            {
+                if (ValidCharFound(textBoxKey.Text))
+                {
+                    int Key = Convert.ToInt32(textBoxKey.Text); //(Ключ должен быть в пределах значений алфавита)
+                    if (Key > -33 && Key < 33)
+                    {
+                        string Text = Convert.ToString(textBoxDecryptText.Text.ToLower()); // храню фразу, которую буду шифровать
+                        string Crypt = ""; // храню результат шифрования
+                        string Space = " ";
+                        string Comma = ",";
+                        string Dot = ".";
+                        string Dash = "-";
+                        string ExclamationMark = "!";
+                        string QuestionMark = "?";
+                        string RusAlphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"; // работаю с русским алфавитом
+                        for (int i = 0; i < Text.Length; i++) // цикл перебора букв шифруемого слова
+                        {
+                            for (int j = 0; j < RusAlphabet.Length; j++) // цикл сравнения каждой буквы с алфавитом
+                            {
+                                if (Text[i] == RusAlphabet[j]) // в случае совпадения, создаю переменную, где храню номер буквы со сдвигом
+                                {
+                                    int end_position = j + Key; // номер буквы + сдвиг по ключу
+
+                                    while (end_position <= RusAlphabet.Length) // чтобы новая буква не уходила за рамки алфавита
+                                    {
+                                        end_position += RusAlphabet.Length;
+                                    }
+
+                                    while (end_position >= RusAlphabet.Length) // чтобы новая буква не уходила за рамки алфавита
+                                    {
+                                        end_position -= RusAlphabet.Length;
+                                    }
+
+                                    Crypt += RusAlphabet[end_position]; // заношу зашифрованную букву в переменную, для ее хранения
+                                }
+                            }
+
+                            if (Text[i] == Convert.ToChar(Space))
+                            {
+                                Crypt += Space;
+                            }
+
+                            if (Text[i] == Convert.ToChar(Comma))
+                            {
+                                Crypt += Comma;
+                            }
+
+                            if (Text[i] == Convert.ToChar(Dot))
+                            {
+                                Crypt += Dot;
+                            }
+
+                            if (Text[i] == Convert.ToChar(Dash))
+                            {
+                                Crypt += Dash;
+                            }
+
+                            if (Text[i] == Convert.ToChar(ExclamationMark))
+                            {
+                                Crypt += ExclamationMark;
+                            }
+
+                            if (Text[i] == Convert.ToChar(QuestionMark))
+                            {
+                                Crypt += QuestionMark;
+                            }
+                        }
+
+                        textBoxEncryptText.Text = Crypt;
+                    }
+                    else
+                        MessageBox.Show("Некорректное значение ключа шифрования!" +
+                           "\nКлюч может принимать значения от -32 до 32", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                }
+                else
+                    MessageBox.Show("Укажите числовые значения в поле Ключ шифрования!", "Ошибка",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            }
+            else
+                MessageBox.Show("Значение Ключа шифрования отсутствует!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonHelp_Click(object sender, EventArgs e)
+        {
+           
+            FormHelp formHelp = new();
+            formHelp.Show();
+        }
+
+        void buttonHelp_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F1)
+            {
+                buttonHelp.Select();
+                buttonHelp_Click(buttonHelp, null);
+            }
+        }
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+        {
+            if (textBoxKey.Text != null && textBoxKey.Text != "")
+            {
+                if (ValidCharFound(textBoxKey.Text))
+                {
+                    int Key = Convert.ToInt32(textBoxKey.Text); //(Ключ должен быть в пределах значений алфавита)
+                    if (Key > -33 && Key < 33)
+                    {                      
+                      string Text = Convert.ToString(textBoxEncryptText.Text.ToLower()); // храню фразу, которую буду шифровать
+                      string Crypt = ""; // храню результат шифрования
+                      string Space = " ";
+                      string Comma = ",";
+                      string Dot = ".";
+                      string Dash = "-";
+                      string ExclamationMark = "!";
+                      string QuestionMark = "?";
+                      string RusAlphabet = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"; // работаю с русским алфавитом
+
+                      for (int i = 0; i < Text.Length; i++) // цикл перебора букв шифруемого слова
+                      {
+                        for (int j = 0; j < RusAlphabet.Length; j++) // цикл сравнения каждой буквы с алфавитом
+                        {
+                           if (Text[i] == RusAlphabet[j]) // в случае совпадения, создаю переменную, где храню номер буквы со сдвигом
+                           {
+                            int end_position = j - Key; // номер буквы + сдвиг по ключу
+
+                                    while (end_position <= RusAlphabet.Length) // чтобы новая буква не уходила за рамки алфавита
+                                    { 
+                                      end_position += RusAlphabet.Length; 
+                                    }
+  
+                                    while (end_position >= RusAlphabet.Length) // чтобы новая буква не уходила за рамки алфавита
+                                    {
+                                      end_position -= RusAlphabet.Length; 
+                                    }
+
+                              Crypt +=RusAlphabet[end_position]; // заношу зашифрованную букву в переменную, для ее хранения
+                           }
+                        }
+
+                            if (Text[i] == Convert.ToChar(Space))
+                            {
+                                Crypt += Space;
+                            }
+
+                            if (Text[i] == Convert.ToChar(Comma))
+                            {
+                                Crypt += Comma;
+                            }
+
+                            if (Text[i] == Convert.ToChar(Dot))
+                            {
+                                Crypt += Dot;
+                            }
+
+                            if (Text[i] == Convert.ToChar(Dash))
+                            {
+                                Crypt += Dash;
+                            }
+
+                            if (Text[i] == Convert.ToChar(ExclamationMark))
+                            {
+                                Crypt += ExclamationMark;
+                            }
+
+                            if (Text[i] == Convert.ToChar(QuestionMark))
+                            {
+                                Crypt += QuestionMark;
+                            }
+                      }
+                      textBoxDecryptText.Text = Crypt;
+                    }
+                    else
+                        MessageBox.Show("Некорректное значение ключа шифрования!" +
+                           "\nКлюч может принимать значения от -32 до 32", "Внимание", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                    MessageBox.Show("Укажите числовые значения в поле Ключ шифрования!", "Ошибка",
+                       MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+                MessageBox.Show("Значение Ключа шифрования отсутствует!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void buttonClearEncryptText_Click(object sender, EventArgs e)
+        {
+            textBoxEncryptText.Text = "";
+        }
+
+        private void buttonClearDecryptText_Click(object sender, EventArgs e)
+        {
+            textBoxDecryptText.Text = "";
+        }
+
+        private void buttonSaveDecryptText_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            File.WriteAllText(saveFileDialog1.FileName, textBoxDecryptText.Text);
+        }
+
+        private void buttonSaveEncryptText_Click_1(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+            File.WriteAllText(saveFileDialog1.FileName, textBoxEncryptText.Text);
+           
+        }
+
+        private void buttonOpenDecryptText_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.ShowDialog();
+                textBoxInput.Text = openFileDialog1.FileName;
+                textBoxDecryptText.Clear();
+                textBoxDecryptText.Text = File.ReadAllText(textBoxInput.Text, Encoding.GetEncoding(65001));
+            }
+            catch
+            {
+                MessageBox.Show("Текстовый документ отсутствует", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonOpenEncryptText_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFileDialog1.ShowDialog();
+                textBoxOutput.Text = openFileDialog1.FileName;
+                textBoxEncryptText.Clear();
+                textBoxEncryptText.Text = File.ReadAllText(textBoxOutput.Text, Encoding.GetEncoding(65001));
+            }
+            catch
+            {
+                MessageBox.Show("Текстовый документ отсутствует", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
+
---
+++ Caesar Cipher/FormHelp.Designer.cs
@@ -0,0 +1,296 @@
+﻿
+namespace CaesarCipher
+{
+    partial class FormHelp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormHelp));
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.labelInfo = new System.Windows.Forms.Label();
+            this.labelInfoButton = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.labelInfoHelp = new System.Windows.Forms.Label();
+            this.labelExitInfo = new System.Windows.Forms.Label();
+            this.labelClearInfo = new System.Windows.Forms.Label();
+            this.labeltextBoxInfo = new System.Windows.Forms.Label();
+            this.labelDecryptText = new System.Windows.Forms.Label();
+            this.labelEncryptText = new System.Windows.Forms.Label();
+            this.labelKey = new System.Windows.Forms.Label();
+            this.labelCryptInfo = new System.Windows.Forms.Label();
+            this.labelSaveInfo = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Location = new System.Drawing.Point(631, 396);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "Закрыть";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            this.buttonClose.Click += new System.EventHandler(this.buttonClose_Click);
+            // 
+            // labelInfo
+            // 
+            this.labelInfo.AutoSize = true;
+            this.labelInfo.Location = new System.Drawing.Point(12, 9);
+            this.labelInfo.Name = "labelInfo";
+            this.labelInfo.Size = new System.Drawing.Size(340, 30);
+            this.labelInfo.TabIndex = 1;
+            this.labelInfo.Text = "Программа разработана в рамках Практикума по WinForms\r\nАгаповым И.Н. в качестве и" +
+    "ндивидуального проекта";
+            // 
+            // labelInfoButton
+            // 
+            this.labelInfoButton.AutoSize = true;
+            this.labelInfoButton.Location = new System.Drawing.Point(12, 105);
+            this.labelInfoButton.Name = "labelInfoButton";
+            this.labelInfoButton.Size = new System.Drawing.Size(201, 15);
+            this.labelInfoButton.TabIndex = 2;
+            this.labelInfoButton.Text = "Справочная инфорация о кнопках:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 127);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Справка";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(10, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Выход";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(10, 185);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(10, 214);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Очистить поле";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // labelInfoHelp
+            // 
+            this.labelInfoHelp.AutoSize = true;
+            this.labelInfoHelp.Location = new System.Drawing.Point(91, 131);
+            this.labelInfoHelp.Name = "labelInfoHelp";
+            this.labelInfoHelp.Size = new System.Drawing.Size(260, 15);
+            this.labelInfoHelp.TabIndex = 7;
+            this.labelInfoHelp.Text = "Открытые окна со справочной информацией";
+            // 
+            // labelExitInfo
+            // 
+            this.labelExitInfo.AutoSize = true;
+            this.labelExitInfo.Location = new System.Drawing.Point(91, 160);
+            this.labelExitInfo.Name = "labelExitInfo";
+            this.labelExitInfo.Size = new System.Drawing.Size(119, 15);
+            this.labelExitInfo.TabIndex = 8;
+            this.labelExitInfo.Text = "Закрыть программу";
+            // 
+            // labelClearInfo
+            // 
+            this.labelClearInfo.AutoSize = true;
+            this.labelClearInfo.Location = new System.Drawing.Point(115, 218);
+            this.labelClearInfo.Name = "labelClearInfo";
+            this.labelClearInfo.Size = new System.Drawing.Size(217, 15);
+            this.labelClearInfo.TabIndex = 10;
+            this.labelClearInfo.Text = "Удалить содержимое текстового поля";
+            // 
+            // labeltextBoxInfo
+            // 
+            this.labeltextBoxInfo.AutoSize = true;
+            this.labeltextBoxInfo.Location = new System.Drawing.Point(10, 309);
+            this.labeltextBoxInfo.Name = "labeltextBoxInfo";
+            this.labeltextBoxInfo.Size = new System.Drawing.Size(248, 15);
+            this.labeltextBoxInfo.TabIndex = 11;
+            this.labeltextBoxInfo.Text = "Справочная инфорация о текстовых полях:";
+            // 
+            // labelDecryptText
+            // 
+            this.labelDecryptText.AutoSize = true;
+            this.labelDecryptText.Location = new System.Drawing.Point(10, 339);
+            this.labelDecryptText.Name = "labelDecryptText";
+            this.labelDecryptText.Size = new System.Drawing.Size(483, 15);
+            this.labelDecryptText.TabIndex = 12;
+            this.labelDecryptText.Text = "В поле \"Дешифрованный текст\" нужно ввести текст, который требуется зашифровать";
+            // 
+            // labelEncryptText
+            // 
+            this.labelEncryptText.AutoSize = true;
+            this.labelEncryptText.Location = new System.Drawing.Point(10, 363);
+            this.labelEncryptText.Name = "labelEncryptText";
+            this.labelEncryptText.Size = new System.Drawing.Size(490, 15);
+            this.labelEncryptText.TabIndex = 13;
+            this.labelEncryptText.Text = "В поле \"Зашифрованный текст\" нужно ввести текст, который требуется расшифровать";
+            // 
+            // labelKey
+            // 
+            this.labelKey.AutoSize = true;
+            this.labelKey.Location = new System.Drawing.Point(10, 389);
+            this.labelKey.Name = "labelKey";
+            this.labelKey.Size = new System.Drawing.Size(589, 30);
+            this.labelKey.TabIndex = 14;
+            this.labelKey.Text = "В поле \"Введите ключ шифрования\" нужно указать целочисленное значение числа сдвиг" +
+    "ов по алфавиту\r\n(ключ шифрования) ";
+            // 
+            // labelCryptInfo
+            // 
+            this.labelCryptInfo.AutoSize = true;
+            this.labelCryptInfo.Location = new System.Drawing.Point(12, 51);
+            this.labelCryptInfo.Name = "labelCryptInfo";
+            this.labelCryptInfo.Size = new System.Drawing.Size(647, 45);
+            this.labelCryptInfo.TabIndex = 15;
+            this.labelCryptInfo.Text = resources.GetString("labelCryptInfo.Text");
+            // 
+            // labelSaveInfo
+            // 
+            this.labelSaveInfo.AutoSize = true;
+            this.labelSaveInfo.Location = new System.Drawing.Point(91, 189);
+            this.labelSaveInfo.Name = "labelSaveInfo";
+            this.labelSaveInfo.Size = new System.Drawing.Size(273, 15);
+            this.labelSaveInfo.TabIndex = 16;
+            this.labelSaveInfo.Text = "Сохранить содержимое текстового поля в файл";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(10, 243);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(93, 23);
+            this.button5.TabIndex = 17;
+            this.button5.Text = "Зашифровать";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(10, 272);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(95, 23);
+            this.button6.TabIndex = 18;
+            this.button6.Text = "Дешифровать";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 247);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(376, 15);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Зашифровать текст, находящийся в поле \"Дешифрованный текст\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(111, 276);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(381, 15);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Расшифровать текст, находящийся в поле \"Зашифрованный текст\"";
+            // 
+            // FormHelp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 434);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.labelSaveInfo);
+            this.Controls.Add(this.labelCryptInfo);
+            this.Controls.Add(this.labelKey);
+            this.Controls.Add(this.labelEncryptText);
+            this.Controls.Add(this.labelDecryptText);
+            this.Controls.Add(this.labeltextBoxInfo);
+            this.Controls.Add(this.labelClearInfo);
+            this.Controls.Add(this.labelExitInfo);
+            this.Controls.Add(this.labelInfoHelp);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelInfoButton);
+            this.Controls.Add(this.labelInfo);
+            this.Controls.Add(this.buttonClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.Name = "FormHelp";
+            this.Text = "Справка";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label labelInfo;
+        private System.Windows.Forms.Label labelInfoButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label labelInfoHelp;
+        private System.Windows.Forms.Label labelExitInfo;
+        private System.Windows.Forms.Label labelClearInfo;
+        private System.Windows.Forms.Label labeltextBoxInfo;
+        private System.Windows.Forms.Label labelDecryptText;
+        private System.Windows.Forms.Label labelEncryptText;
+        private System.Windows.Forms.Label labelKey;
+        private System.Windows.Forms.Label labelCryptInfo;
+        private System.Windows.Forms.Label labelSaveInfo;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
